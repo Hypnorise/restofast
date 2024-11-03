2fc483a193d14d03da590e3c8486fb4bbac97c13
RENAUD <renaud.harrys@outlook.com>
2024-11-03 17:06:05 +0300

    Experimentations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('api')->group(function () {
+    Route::resource('products', ProductsController::class);
 });
